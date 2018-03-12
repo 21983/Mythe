1102e90f57d8e70da96ff627f7ff9ad9d16c0659
Yaruno292 <Yarunoyo@outlook.com>
2018-03-12 14:23:37 +0100

    Pause menu
    Added a menu inputs script
    Added a pause menu

---
+++ Assets/Scripts/Menu/Pause.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _pauseUI;
+
+	// Use this for initialization
+	void Start () {
+        _pauseUI.SetActive(false);
+	}
+	
+    public void OnPause()
+    {
+        Debug.Log("paused..");
+        _pauseUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+	public void Resume()
+    {
+        Debug.Log("unpause");
+        _pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
