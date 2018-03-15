cc19d0d76b92220774182dbb67c3228db6e8aa40
Joppe van Basten <21983@ma-web.nl>
2018-03-15 12:27:21 +0100

    Platform spawnen

---
+++ Assets/Scripts/Joppe/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    [SerializeField]
+    private int speed = 2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += Vector3.up * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/Joppe/DeletePlatform.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeletePlatform : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject cam;
+
+	void Start () {
+        cam = GameObject.Find("Main Camera");
+	}
+
+	void Update () {
+		if(cam.transform.position.y >= gameObject.transform.position.y + 20)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
