0f242af41cca68301689f6bb8fc6a73e664e747e
Bora Oksal <boraoxal@gmail.com>
2018-04-19 13:35:35 +0200

    merging

--- Assets/Scripts/Menu/CharSelectInputs.cs
+++ Assets/Scripts/Menu/CharSelectInputs.cs
@@ -2,25 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharSelectInputs : MonoBehaviour {
+public class CharSelectInputs : MonoBehaviour
+{
+    //These 2 are from me, comment them out again when testing
+    //Original code has the X + Z keycodes
+    [SerializeField] KeyCode join;
+    [SerializeField] KeyCode leave;
 
     public JoinGame joinGame;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         joinGame = GetComponent<JoinGame>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown(KeyCode.X))
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.X))
+        if (Input.GetKeyDown(join))
         {
             joinGame.OnJoin();
         }
 
-        if (Input.GetKeyDown(KeyCode.Z))
+        //if (Input.GetKeyDown(KeyCode.Z))
+        if (Input.GetKeyDown(leave))
         {
             joinGame.OnLeave();
         }
     }
 }
+
--- Assets/Scripts/Players/Player1Controller.cs
+++ Assets/Scripts/Players/Player1Controller.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,45 +7,46 @@ public class Player1Controller : MonoBehaviour
     [SerializeField] private string Joystick;
     [SerializeField] float moveSpeed;
     private PlayerAttack _playerAttack;
+    private float translation;
     [SerializeField] KeyCode jump;
     [SerializeField] KeyCode punch;
     public bool stunned;
+
     private void Start()
     {
         _playerAttack = GetComponent<PlayerAttack>();
     }
 
     private void Update()
-<<<<<<< HEAD
-    { 
+    {
         float translation = Input.GetAxis("LeftJoystickX_P1") * moveSpeed;
         //float translation = Input.GetAxis("Horizontal") * moveSpeed;
-        if(translation < 0)
+        if (translation < 0)
         {
             gameObject.GetComponent<PlayerAttack>().ChangeHitbox("left");
         }
-        else if(translation > 0)
-=======
-    {
-        if (!stunned)
->>>>>>> b67b4e1ffdd0f431aa5642cfdac773c651f50b24
+        else if (translation > 0)
         {
-            float translation = Input.GetAxis(Joystick) * moveSpeed;
-            //float translation = Input.GetAxis("Horizontal") * moveSpeed;
-            if (translation < 0)
+            if (!stunned)
             {
-                _playerAttack.ChangeHitbox("left");
-            }
-            else if (translation > 0)
-            {
-                _playerAttack.ChangeHitbox("right");
-            }
+                float translation = Input.GetAxis(Joystick) * moveSpeed;
+                //float translation = Input.GetAxis("Horizontal") * moveSpeed;
+                if (translation < 0)
+                {
+                    _playerAttack.ChangeHitbox("left");
+                }
+                else if (translation > 0)
+                {
+                    _playerAttack.ChangeHitbox("right");
+                }
 
-            translation *= Time.deltaTime;
-            transform.position += new Vector3(translation, 0, 0);
+                translation *= Time.deltaTime;
+                transform.position += new Vector3(translation, 0, 0);
 
-            gameObject.GetComponent<PlayerJump>().Jump(jump);
-            _playerAttack.Attack(punch);
+                gameObject.GetComponent<PlayerJump>().Jump(jump);
+                _playerAttack.Attack(punch);
+            }
         }
     }
 }
+*/
